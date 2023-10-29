b28511171198f6f46c5f7efeeb6b62d2c226cd9e
francois <francois.cokelaer@gmail.com>
2023-10-29 17:31:05 +0100

    update package

--- static/index.js
+++ static/index.js
@@ -7,6 +7,7 @@ async function run() {
     wasm = await init();
     memory = wasm.memory;
 
+
     const CELL_SIZE = 12; // px
     const GRID_COLOR = "#CCCCCC";
     const DEAD_COLOR = "#FFFFFF";
@@ -58,5 +59,76 @@ async function run() {
     const isPaused = () => {
       return animationId === null;
     };
+    
+    const play = () => {
+      playPauseButton.textContent = "⏸";
+      renderLoop();
+    };
+    
+    const pause = () => {
+      playPauseButton.textContent = "▶";
+      cancelAnimationFrame(animationId);
+      animationId = null;
+    };
+    
+    playPauseButton.addEventListener("click", event => {
+      if (isPaused()) {
+        play();
+      } else {
+        pause();
+      }
+    });
+    
+    
+    const drawGrid = () => {
+      ctx.beginPath();
+      ctx.strokeStyle = GRID_COLOR;
+    
+      // Vertical lines.
+      for (let i = 0; i <= width; i++) {
+        ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+        ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+      }
+    
+      // Horizontal lines.
+      for (let j = 0; j <= height; j++) {
+        ctx.moveTo(0,                           j * (CELL_SIZE + 1) + 1);
+        ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+      }
+    
+      ctx.stroke();
+    };
+    
+    const getIndex = (row, column) => {
+      return row * width + column;
+    };
+    
+    const drawCells = () => {
+      const cellsPtr = universe.cells();
+      const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+    
+      ctx.beginPath();
+    
+      for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+          const idx = getIndex(row, col);
+    
+          ctx.fillStyle = cells[idx] === Cell.Dead
+            ? DEAD_COLOR
+            : ALIVE_COLOR;
+    
+          ctx.fillRect(
+            col * (CELL_SIZE + 1) + 1,
+            row * (CELL_SIZE + 1) + 1,
+            CELL_SIZE,
+            CELL_SIZE
+          );
+        }
+      }
+    
+      ctx.stroke();
+    };
+    
+    play();
 }
 run();
